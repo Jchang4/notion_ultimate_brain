017e939964c8a6785b6bb35b6754a7f7e5027482
Justin Chang <justinchangmusic@gmail.com>
2022-12-17 15:58:18 -0500

    Add property container

---
+++ notion_helpers/data_types/notion_properties/property.py
@@ -0,0 +1,56 @@
+from enum import Enum
+from typing import Any, Dict
+
+
+class PropertyTypes(str, Enum):
+    TITLE = "title"
+    RICH_TEXT = "rich_text"
+    NUMBER = "number"
+    SELECT = "select"
+    MULTI_SELECT = "multi_select"
+    DATE = "date"
+    PEOPLE = "people"
+    FILES = "files"
+    CHECKBOX = "checkbox"
+    URL = "url"
+    EMAIL = "email"
+    PHONE_NUMBER = "phone_number"
+    FORMULA = "formula"
+    RELATION = "relation"
+    ROLLUP = "rollup"
+    CREATED_TIME = "created_time"
+    CREATED_BY = "created_by"
+    LAST_EDITED_TIME = "last_edited_time"
+    LAST_EDITED_BY = "last_edited_by"
+    STATUS = "status"
+
+    def __repr__(self) -> str:
+        return f'<PropertyType "{self.value}">'
+
+
+class Property:
+    _raw: Dict[str, Any]
+    id: str
+    name: str
+    type: PropertyTypes
+    value: Dict[str, Any]
+
+    def __init__(self, json_data: Dict[str, Any]):
+        self._raw = json_data
+
+        self.id = self._raw["id"]
+        self.name = self._raw.get("name", "")
+        self.type = PropertyTypes(self._raw["type"])
+
+        # Always run this last so we can have the above
+        # properties available in self
+        self.value = self.get_value(self._raw[self.type])
+
+    def get_value(self, payload: Any):
+        raise NotImplementedError()
+
+    def __repr__(self) -> str:
+        str_repr = f'id="{self.id}" name="{self.name}"'
+        if issubclass(self.__class__, Property):
+            return str_repr
+        return f'<Property {str_repr} type="{str(self.type.value)}">'
