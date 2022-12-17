46a8f4a4460c33e0cef053d23c9bf187f0deaf47
Justin Chang <justinchangmusic@gmail.com>
2022-12-17 15:57:42 -0500

    Add notion database class, no property integration yet

---
+++ notion_helpers/data_types/notion_database.py
@@ -0,0 +1,36 @@
+from typing import Any, Dict
+
+from notion_helpers.helpers import get_plain_text_from_title
+
+
+class Database:
+    _raw: Dict[str, Any]
+
+    id: str
+    title: str
+    parent: Dict[str, Any]
+    url: str
+    archived: bool
+    properties: Dict[str, Any]
+
+    def __init__(self, json_data: Dict[str, Any]):
+        self._raw = json_data
+
+        self.id = self._raw["id"]
+        self.title = self.get_database_title(self._raw)
+        self.parent = self._raw["parent"]
+        self.url = self._raw["url"]
+        self.archived = self._raw["archived"]
+        self.properties = self._raw["properties"]
+
+    @staticmethod
+    def get_database_title(database: Dict[str, Any]) -> str:
+        return get_plain_text_from_title(database["title"])
+
+    def __repr__(self) -> str:
+        db_repr = f'id="{self.id}" title="{self.title}" archived={self.archived}'
+
+        if issubclass(self.__class__, Database):
+            return db_repr
+
+        return f"<Database {db_repr}>"
