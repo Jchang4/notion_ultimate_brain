6b8f557a5d36e3e06501597b2e69c249cdc60f77
Justin Chang <justinchangmusic@gmail.com>
2022-12-17 15:52:47 -0500

    Add some notion helpers

---
+++ notion_helpers/helpers.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+from notion_client import Client
+
+NOTION_TOKEN = os.environ["NOTION_TOKEN"]
+
+
+def create_client(log_level: int = logging.WARNING) -> Client:
+    return Client(auth=NOTION_TOKEN, log_level=log_level)
+
+
+def get_plain_text_from_title(title):
+    return " ".join([t["plain_text"] for t in title])
+
+
+def get_page_title(page):
+    title = page["properties"]["Title"]["title"]
+    return get_plain_text_from_title(title)
