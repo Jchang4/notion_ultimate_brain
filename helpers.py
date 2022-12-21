6a1f2d73cd572b3ae731d1683e88be1ef9dcf5ab
Justin Chang <justinchangmusic@gmail.com>
2022-12-21 11:24:07 -0500

    Remove unused files, will clean up in a single package

--- helpers.py
+++ helpers.py
@@ -1,8 +1,31 @@
-import operator
+from datetime import datetime, timedelta
 from functools import reduce
-from typing import Any, Callable, Dict, List, Optional, TypeVar
+from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, TypeVar
+
+from dateutil import parser
 
 T = TypeVar("T")
+
+
+def first(items: Any):
+    if not items:
+        return None
+    if not isinstance(items, Sequence):
+        items = list(items)
+    return items[0]
+
+
+def firstx(items: Any):
+    item = first(items)
+    assert item is not None
+    return item
+
+
+def flatten(arrs):
+    result = []
+    for arr in arrs:
+        result.extend(arr)
+    return result
 
 
 def dict_by(items: List[T], prop: str) -> Dict[Any, T]:
@@ -30,3 +53,14 @@ def find(element, json, default: Optional[T] = None) -> T:
     for p in props[:-1]:
         json = json.get(p, {})
     return json.get(props[-1], default)
+
+
+def get_day_midnight(strftime: str = "", offset_days: int = 0) -> datetime:
+    date = parser.parse(strftime) if strftime else datetime.now()
+    date += timedelta(days=offset_days)
+    return date.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def get_start_and_end_of_day(offset_days: int = 0) -> Tuple[datetime, datetime]:
+    day = get_day_midnight(offset_days=offset_days)
+    return day, day + timedelta(days=1)
--- notion_helpers/data_types/notion_properties/all.py
+++
@@ -1,8 +0,0 @@
-from notion_helpers.data_types.notion_properties.date_property import (
-    CreatedTimeProperty,
-    DateProperty,
-)
-from notion_helpers.data_types.notion_properties.helpers import get_properties
-from notion_helpers.data_types.notion_properties.number_property import NumberProperty
-from notion_helpers.data_types.notion_properties.property import Property, PropertyTypes
-from notion_helpers.data_types.notion_properties.url_property import UrlProperty
